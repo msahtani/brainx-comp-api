8cee1da8926bdbdfa1a62863310041f45629783b
msahtani <mohcine.sahtani@gmail.com>
2023-03-20 22:22:22 +0000

    merging `generate_id.ts` and `checks.ts` into one file called `utils.ts`

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,16 @@
+import * as crypto from 'crypto'
 
 const MIN = 10
+
+export function generateId(bytes: number = 24): string {
+    
+    let result = ""
+    crypto.getRandomValues(new Uint8Array(bytes))
+        .forEach(
+            byte => result += byte.toString(16)
+        )
+    return result
+}
 
 export function checkEmail(email: string): boolean{
     const pattern = /[a-zA-Z](\w|\.)+@\w{3,}\.\w{2,}/gi
